c01d7c3f954d32ad43981a7ba836eb64729e0f08
RoseLand96 <1716210001@utec-tgo.edu.mx>
2017-03-31 07:34:40 -0700

    Update calendario.php

--- calendario.php
+++ calendario.php
@@ -9,7 +9,7 @@ $db_host = "if0ck476y7axojpg.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
 mysqli_select_db($connection,$db_name) or die("Error al seleccionar la base de datos:".mysqli_error());
     @mysqli_query("SET NAMES 'utf8'");
 
-$sql_query = "SELECT * FROM imagen";
+$sql_query = "SELECT * FROM calendario;";
 $result = mysqli_query($connection,$sql_query);
 $rows = array();
 while($r = mysqli_fetch_assoc($result)) {
